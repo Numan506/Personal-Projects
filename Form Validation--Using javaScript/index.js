39ef401b2a004bf39a92ab0c1cb4541727beab78
Abu Jakaria Md Numan <78308167+Numan506@users.noreply.github.com>
2021-09-26 23:16:30 -0700

    Add files via upload

--- Form Validation--Using javaScript/index.js
+++ Form Validation--Using javaScript/index.js
@@ -7,43 +7,55 @@ var password1 = document.querySelector("#password1")
 
 // Event System
 form.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    
- 
-// Now Show Error Message
-if(username.value==""){
-    errorFunction(username,"Username is Required")
-}else{
-    successFunction(username)
-};
+e.preventDefault();
+checkRequired([username,email,password,password1]);
 
-if(email.value==""){
-    errorFunction(email,"Email is Required")
-    
-}else if(!validateEmail(email.value)){
-      errorFunction(email,"Email is not valid")
+checkLength(username, 5, 20);
+checkLength(password, 6, 15);
+emailTest(email);
+checkPassword(password,password1)
+
+
+});
+
+
+
+
+
+// checkRequired
+function checkRequired(inputArray){
+    inputArray.forEach(function(input){
+        if(input.value.trim()===""){
+         errorFunction(input,`${getFieldName(input)} is required`)
+        }
+        else{
+            successFunction(input)
+        }
+    })
+ 
+ }
+
+ // checkLength
+
+function checkLength(input,min,max){
+    if(input.value.length<min){
+        errorFunction(input,`${getFieldName(input)} must be at list ${min} charecter`)
+    }else if(input.value.length>max){
+         errorFunction(input,`${getFieldName(input)} must be less then ${max} charecter`)
+    }else{
+        successFunction(input)
+    }
+
 }
 
-else{
-    successFunction(email)
-};
 
-if(password.value==""){
-    errorFunction(password,"Password is Required")
-}else{
-    successFunction(password)
-};
+//  getFieldName
 
-if(password1.value==""){
-    errorFunction(password1,"Password1 is Required")
-}else{
-    successFunction(password1)
-};
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase()+input.id.slice(1);
 
+}
 
-
-
-})
 
 
 // errorFunction
@@ -60,15 +72,26 @@ function errorFunction(input,message){
 function successFunction(input){
     var formControl = input.parentElement;
     formControl.className = "form-control success "
-    var text = input.target.value
-    console.log(text)
+    
 };
 
 // email validity
 
-function validateEmail(email) {
+function emailTest(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    
+    if(re.test(input.value.trim())){
+        successFunction()
+    }else{
+        errorFunction(input,"email is not valid")
+    }
 }
 
+// confirm password
+
+function checkPassword(input1,input2){
+          if(input1.value !== input2.value){
+            errorFunction(input2,"password1 do not match")
+          }
+}
 
